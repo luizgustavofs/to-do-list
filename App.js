092fb03a67355c674de93ed11b7c5ccf7d774dc4
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2021-11-18 20:41:01 -0300

    🚧 Corpo da tela construido

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
+  const [list, setList] = useState([])
+  const [inputValue, setInputValue] = useState('')
 
   return (
-    <View styles={styles.container}>
-      <Text>To Do List</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>To do list</Text>
+      <Text style={styles.subtitle}>1 item adicionado</Text>
+      <FlatList style={styles.list}></FlatList>
+      <View style={styles.inputContainer}>
+        <View style={styles.customInput}>
+        <TextInput
+          style={styles.input}
+          placeholder="insira algo"
+          onChangeText={setInputValue}
+          value={inputValue}
+        />
+        <TouchableOpacity
+          style={styles.button}
+        />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,5 +33,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F1F4F9',
     padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: '#9699A2',
+    fontSize: 16,
+  },
+  list: {
+  },
+  inputContainer: {
+    backgroundColor: `#F1F4F9`,
+    height: 50,
+    
+  },
+  customInput:{
+    flexDirection: `row`,
+    padding: 5,
+    height: 40,
+    borderWidth: 1,
+    alignItems: 'center',
+    marginTop: 16,
+    borderRadius: 8,
+    borderColor: '#ccc'
+  },
+  input: {
+    height: 40,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#ccc',
+    height: 30,
+    width: 30,
+    borderRadius: 8,
   },
 });
